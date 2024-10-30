52c4442789a4632dec752926c084ce86351e8c72
rjpr <ricardo.parrague@usm.cl>
2024-10-30 16:24:01 -0300

    acordeon comentarios

--- games_opinion/src/components/Juego.vue
+++ games_opinion/src/components/Juego.vue
@@ -26,34 +26,20 @@
       </v-col>
     </v-row>
     <v-container v-if="this.juegos[this.IdJuego].comentarios">
-      <v-row
-        v-for="comentario in this.juegos[this.IdJuego].comentarios"
+
+      <v-row justify="center">
+    <v-expansion-panels accordion>
+      <v-expansion-panel
+       v-for="comentario in this.juegos[this.IdJuego].comentarios"
         :key="comentario.nombre"
       >
-        <v-col>
-          <v-card class="mx-auto">
-            <v-card-title>
-              Opinión creada por: {{ comentario.nombre }}
-            </v-card-title>
-
-            <v-card-actions>
-              <v-btn icon @click="show = !show">
-                <v-icon>{{
-                  show ? "mdi-chevron-up" : "mdi-chevron-down"
-                }}</v-icon>
-              </v-btn>
-            </v-card-actions>
-
-            <v-expand-transition>
-              <div v-show="show">
-                <v-divider></v-divider>
-
-                <v-card-text> {{ comentario.opinion }}</v-card-text>
-              </div>
-            </v-expand-transition>
-          </v-card>
-        </v-col>
-      </v-row>
+        <v-expansion-panel-header>Opinión creada por: {{ comentario.nombre }}</v-expansion-panel-header>
+        <v-expansion-panel-content>
+          {{ comentario.opinion }}
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+  </v-row>
     </v-container>
   </v-container>
 </template>
